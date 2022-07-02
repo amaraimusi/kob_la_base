b1f0ffc3571fd438606206b679c218bd6d363e56
amaraimusi <amaraimusi@gmail.com>
2022-07-02 13:02:47 +0900

    ログインコントローラのカスタマイズ、web.phpのルートに追記。

--- dev/routes/web.php
+++ dev/routes/web.php
@@ -13,10 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//■■■□□□■■■□□□
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Auth::routes();
+Route::get('/', 'App\Http\Controllers\DashboardController@index');
+Route::get('dashboard', 'App\Http\Controllers\DashboardController@index');
+Route::get('/home', 'App\Http\Controllers\DashboardController@index');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Auth::routes(); // ログイン画面関連
+
